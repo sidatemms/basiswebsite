983f8e941e6f23ec7e9b86c0a89037febf8ec945
sidatemms <145342377+sidatemms@users.noreply.github.com>
2024-09-18 15:21:13 +0200

    update

--- README.md
+++ README.md
@@ -76,10 +76,10 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
   <summary>uitwerken na afloop 3<sup>e</sup> werkgroep</summary>
 
   ### de hele pagina: 
-  <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="breakdown van de hele pagina">
+  <img src="images/breakdownschets.png" width="375px" alt="breakdown van de hele pagina">
 
   ### dynamisch deel (bijv menu): 
-  <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="breakdown van een dynamisch deel">
+  <img src="images/dynamische_deel.png" width="375px" alt="breakdown van een dynamisch deel">
 
   ### wellicht nog een dynamisch deel (bijv filter): 
   <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="breakdown van nog een dynamisch deel">
--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,112 @@
 // JavaScript Document
 console.log("hi");
+
+function createCaroCarrousel(carrouselID) {
+	let carrousel = document.querySelector("#"+carrouselID);
+  let carrouselElementsContainer = carrousel.querySelector(":scope > ul");
+	let carrouselElements = carrouselElementsContainer.querySelectorAll("li");
+  let bolletjes = carrousel.querySelectorAll(":scope > nav a");
+	
+  
+  /****************/
+	/* DE BOLLETJES */
+	/****************/
+  
+  // de bolletjes activeren
+  function iniBolletjes() {
+    for (bolletje of bolletjes) {
+			// elk bolletje laten luisteren naar kliks
+      bolletje.addEventListener("click", function(e){
+				// als er geklikt wordt
+        
+				// de default-actie (de link volgen) niet uitvoeren
+        // anders verplaatst de hele carrousel naar boven in het scherm
+				e.preventDefault();
+
+				// het nieuwe element opzoeken
+				let newElement = carrousel.querySelector(this.hash);
+        
+        // de linker offset van het nieuwe element bepalen 
+        let newElementOffset = newElement.offsetLeft;
+
+        // de carousel naar de berekende positie scrollen
+        carrouselElementsContainer.scrollTo({
+          left: newElementOffset
+        });
+        
+        // nieuwe element current element maken
+		    updateCurrentElement(newElement);
+        
+        // de bolletjes updaten
+		    updateBolletjes(newElement);
+      });
+    }
+	}
+  
+  
+  /*****************/
+	/* START POSITIE */
+	/*****************/
+  
+	// het eerste element en bolletje actief maaken
+  function iniStartPosition() {
+    // eerste element current maken
+    carrouselElements[0].classList.add("current");
+    // eerste bolletje current maken
+		bolletjes[0].classList.add("current");
+		// aan het begin van de container starten
+    carrouselElementsContainer.scrollLeft = 0;
+  }
+  
+  
+  /*********************/
+	/* ALGEMENE FUNCTIES */
+	/*********************/
+  
+  ////////////////////////////
+	// update current element //
+	function updateCurrentElement(newElement) {
+		// het huidige current element opzoeken
+		let currentElement = carrousel.querySelector(":scope > ul > .current");
+		// de class current verwijderen
+		currentElement.classList.remove("current");
+
+		// aan nieuwe element de class current toevoegen
+		newElement.classList.add("current");
+	}
+
+  
+  //////////////////////
+  // update bolletjes //
+  function updateBolletjes(newElement){
+		// het huidige current bolletje opzoeken
+		let currentBolletje = carrousel.querySelector(":scope > nav .current");
+		// de class current verwijderen
+		currentBolletje.classList.remove("current");
+		
+		// het nieuwe bolletje opzoeken
+    let newBolletje = carrousel.querySelector("a[href='#"+newElement.id+"']");
+		// de class current toevoegen
+		newBolletje.classList.add("current");
+  }
+
+  
+
+	// de bolletjes activeren
+  iniBolletjes();	
+  // de carrousel bij het begin starten
+  iniStartPosition();
+}
+
+
+/************************/
+/* DE CARROUSEL CREËREN */
+/************************/
+
+// nadat de pagina geladen is, de carrousels activeren
+(function() {
+  // hier de id gebruiken van de section in de html
+  createCaroCarrousel("justBolletjes");
+  //je kunt hier ook meerdere carrousellen activeren
+})();
+  
